ae8b44c297632e84b93aa9dc367956b8eced8897
DenisProkofiev <68288272+DenisProkofiev@users.noreply.github.com>
2021-01-30 13:41:29 +0400

    add .gitignore

--- src/main/java/Numbers/MainClass.java
+++ src/main/java/Numbers/MainClass.java
@@ -1,7 +1,15 @@
 package Numbers;
 
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class MainClass {
     public static void main(String[] args) {
         NumbersGame numbersGame = new NumbersGame();
+
     }
 }
